e4f5524db473feca3f20a77f4dd747d7e244094b
nifle3 <kuper116rus.2005@gmail.com>
2024-03-23 14:05:10 +0300

    add: Erro page

---
+++ src/pages/error/error.tsx
@@ -0,0 +1,22 @@
+import "./error.css"
+
+function Error({errorCode, errorShortInfo , errorInfo } :
+                   {errorCode : number, errorShortInfo : string , errorInfo : string}) {
+
+    errorShortInfo = errorShortInfo.toUpperCase();
+    errorInfo = errorInfo.toUpperCase();
+
+    return (
+    <div className={"not-found-container"}>
+        <h3>{errorShortInfo}</h3>
+        <div className={"not-found-container1"}>
+            <h1 className={"not-found-text1"}>{errorCode}</h1>
+        </div>
+        <div className={"not-found-container2"}>
+            <h2 className={"not-found-text2"}>{errorInfo}</h2>
+        </div>
+    </div>
+    )
+}
+
+export default Error;
